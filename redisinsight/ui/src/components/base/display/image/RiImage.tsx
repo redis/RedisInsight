1553f1a2044f5851e5803027f2210b1d5108d039
Dijana Antovska <dijana.antovska@redis.com>
2025-07-28 14:03:41 +0300

    rename RiImage file

--- redisinsight/ui/src/components/base/display/image/RiImage.tsx
+++ redisinsight/ui/src/components/base/display/image/RiImage.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { RiImageProps, StyledImage } from './image.styles'
 
-const RiImage = ({ size, src, alt, loading, ...rest }: RiImageProps) => (
+const RiImage = ({ size, src, alt, ...rest }: RiImageProps) => (
   <StyledImage src={src} alt={alt} size={size} {...rest} />
 )
 
