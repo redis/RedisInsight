cf0fbcdf07d2ebdc42f973f218ea285ed0bb727b
Dijana Antovska <dijana.antovska@redis.com>
2025-07-28 16:06:41 +0300

    fix import Image -> RiImage

--- redisinsight/ui/src/components/base/display/index.ts
+++ redisinsight/ui/src/components/base/display/index.ts
@@ -1,6 +1,6 @@
 import Loader from './loader/Loader'
 import ProgressBarLoader from './progress-bar/ProgressBarLoader'
-import RiImage from './image/Image'
+import RiImage from './image/RiImage'
 
 export { Loader, ProgressBarLoader, RiImage }
 export { RICollapsibleNavGroup } from './collapsible-nav-group/RICollapsibleNavGroup'
