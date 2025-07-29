d34b9e1dd2b3e99ceea257a23513c702d4a60f50
Dijana Antovska <dijana.antovska@redis.com>
2025-07-29 14:16:05 +0300

    replace the accordion component; change some styling to look nice with the accordion

--- redisinsight/ui/src/packages/redistimeseries-app/src/components/Chart/ChartConfigForm.tsx
+++ redisinsight/ui/src/packages/redistimeseries-app/src/components/Chart/ChartConfigForm.tsx
@@ -13,6 +13,7 @@ import {
   Y_LABEL_MAX_LENGTH,
   TITLE_MAX_LENGTH,
 } from './constants'
+import { RiAccordion } from 'uiSrc/components/base/display/accordion/RiAccordion'
 
 const NewEnumSelect = ({
   selected,
@@ -43,108 +44,106 @@ export default function ChartConfigForm(props: ChartConfigFormProps) {
 
   return (
     <form className="chart-config-form">
-      <div className="chart-top-form">
-        <NewEnumSelect
-          values={Object.keys(GraphMode)}
-          selected={value.mode}
-          onClick={(v) => onChange('mode', v)}
-        />
-        <SwitchInput
-          title="Staircase"
-          checked={value.staircase}
-          onCheckedChange={(checked) => onChange('staircase', checked)}
-        />
-        <SwitchInput
-          title="Fill"
-          checked={value.fill}
-          onCheckedChange={(checked) => onChange('fill', checked)}
-        />
-        <EuiAccordion
-          arrowDisplay="right"
-          forceState={moreOptions ? 'open' : 'closed'}
-          onToggle={(isOpen) => setMoreOptions(isOpen)}
-          buttonContent={moreOptions ? 'Less options' : 'More options'}
-        >
-          <span></span>
-        </EuiAccordion>
-      </div>
-      {moreOptions && (
-        <div className="more-options">
-          <section>
-            <FormFieldset legend={{ children: 'Title' }}>
-              <EuiFieldText
-                placeholder="Title"
-                value={value.title}
-                onChange={(e) => onChange('title', e.target.value)}
-                aria-label="Title"
-                maxLength={parseInt(TITLE_MAX_LENGTH)}
-              />
-            </FormFieldset>
-            <FormFieldset legend={{ children: 'X axis Label' }}>
-              <EuiFieldText
-                placeholder="X axis label"
-                value={value.xlabel}
-                onChange={(e) => onChange('xlabel', e.target.value)}
-                aria-label="X Label"
-                maxLength={parseInt(X_LABEL_MAX_LENGTH)}
-              />
-            </FormFieldset>
-          </section>
-          <section>
-            <div className="right-y-axis">
-              <div className="switch-wrapper">
-                <SwitchInput
-                  title="Use Right Y Axis"
-                  checked={value.yAxis2}
-                  onCheckedChange={(checked) => onChange('yAxis2', checked)}
-                />
-              </div>
-              {value.yAxis2 && (
-                <div className="y-axis-2">
-                  {Object.keys(value.keyToY2Axis).map((key) => (
-                    <div className="y-axis-2-item">
-                      <div>{key}</div>
-                      <EuiButtonGroup
-                        buttonSize="compressed"
-                        options={['left', 'right'].map((v: string) => ({
-                          id: v,
-                          label: v,
-                        }))}
-                        onChange={(id) =>
-                          onChange('keyToY2Axis', {
-                            ...value.keyToY2Axis,
-                            [key]: id === 'right',
-                          })
-                        }
-                        idSelected={
-                          value.keyToY2Axis[key] === true ? 'right' : 'left'
-                        }
-                        isFullWidth
-                      />
-                    </div>
-                  ))}
-                </div>
-              )}
-            </div>
-          </section>
-          <section className="y-axis-config">
-            <YAxisConfigForm
-              label="Left Y Axis"
-              onChange={(v: any) => onChange('yAxisConfig', v)}
-              isLeftYAxis={true}
-              value={value.yAxisConfig}
-            />
-            {value.yAxis2 && (
-              <YAxisConfigForm
-                label="Right Y Axis"
-                onChange={(v: any) => onChange('yAxis2Config', v)}
-                isLeftYAxis={false}
-                value={value.yAxis2Config}
-              />
-            )}
-          </section>
+        <div className="chart-form-top">
+          <NewEnumSelect
+            values={Object.keys(GraphMode)}
+            selected={value.mode}
+            onClick={(v) => onChange('mode', v)}
+          />
+          <SwitchInput
+            title="Staircase"
+            checked={value.staircase}
+            onCheckedChange={(checked) => onChange('staircase', checked)}
+          />
+          <SwitchInput
+            title="Fill"
+            checked={value.fill}
+            onCheckedChange={(checked) => onChange('fill', checked)}
+          />
         </div>
-      )}
+        <RiAccordion
+          className="chart-form-accordion"
+          collapsible
+          open={moreOptions}
+          onOpenChange={setMoreOptions}
+          label={moreOptions ? 'Less options' : 'More options'}
+          content={        
+          <div className="more-options">
+            <section>
+              <FormFieldset legend={{ children: 'Title' }}>
+                <EuiFieldText
+                  placeholder="Title"
+                  value={value.title}
+                  onChange={(e) => onChange('title', e.target.value)}
+                  aria-label="Title"
+                  maxLength={parseInt(TITLE_MAX_LENGTH)}
+                />
+              </FormFieldset>
+              <FormFieldset legend={{ children: 'X axis Label' }}>
+                <EuiFieldText
+                  placeholder="X axis label"
+                  value={value.xlabel}
+                  onChange={(e) => onChange('xlabel', e.target.value)}
+                  aria-label="X Label"
+                  maxLength={parseInt(X_LABEL_MAX_LENGTH)}
+                />
+              </FormFieldset>
+            </section>
+            <section>
+              <div className="right-y-axis">
+                <div className="switch-wrapper">
+                  <SwitchInput
+                    title="Use Right Y Axis"
+                    checked={value.yAxis2}
+                    onCheckedChange={(checked) => onChange('yAxis2', checked)}
+                  />
+                </div>
+                {value.yAxis2 && (
+                  <div className="y-axis-2">
+                    {Object.keys(value.keyToY2Axis).map((key) => (
+                      <div className="y-axis-2-item">
+                        <div>{key}</div>
+                        <EuiButtonGroup
+                          buttonSize="compressed"
+                          options={['left', 'right'].map((v: string) => ({
+                            id: v,
+                            label: v,
+                          }))}
+                          onChange={(id) =>
+                            onChange('keyToY2Axis', {
+                              ...value.keyToY2Axis,
+                              [key]: id === 'right',
+                            })
+                          }
+                          idSelected={
+                            value.keyToY2Axis[key] === true ? 'right' : 'left'
+                          }
+                          isFullWidth
+                        />
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            </section>
+            <section className="y-axis-config">
+              <YAxisConfigForm
+                label="Left Y Axis"
+                onChange={(v: any) => onChange('yAxisConfig', v)}
+                isLeftYAxis={true}
+                value={value.yAxisConfig}
+              />
+              {value.yAxis2 && (
+                <YAxisConfigForm
+                  label="Right Y Axis"
+                  onChange={(v: any) => onChange('yAxis2Config', v)}
+                  isLeftYAxis={false}
+                  value={value.yAxis2Config}
+                />
+              )}
+            </section>
+          </div>}
+        />
     </form>
   )
 }
