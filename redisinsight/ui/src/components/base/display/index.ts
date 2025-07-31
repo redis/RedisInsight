3016ee7217949944f7a24803b410cd90634ef881
dantovska <dijana.antovska@redis.com>
2025-07-31 10:20:14 +0300

    RI-7214: Replace EUI loading logo with custom bouncing logo component (#4768)
    * create loading logo component
    * add sizing to the component

--- redisinsight/ui/src/components/base/display/index.ts
+++ redisinsight/ui/src/components/base/display/index.ts
@@ -1,8 +1,9 @@
 import Loader from './loader/Loader'
 import ProgressBarLoader from './progress-bar/ProgressBarLoader'
 import RiImage from './image/RiImage'
+import RiLoadingLogo from './loading-logo/RiLoadingLogo'
 
-export { Loader, ProgressBarLoader, RiImage }
+export { Loader, ProgressBarLoader, RiImage, RiLoadingLogo }
 export { RICollapsibleNavGroup } from './collapsible-nav-group/RICollapsibleNavGroup'
 
 export type { RICollapsibleNavGroupProps } from './collapsible-nav-group/RICollapsibleNavGroup'
