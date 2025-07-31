50fa206f3cf4fcd0fc16b8129d3ad9b88c925e7e
Dijana Antovska <dijana.antovska@redis.com>
2025-07-31 10:05:20 +0300

    return the exported type and format file

--- redisinsight/ui/src/components/base/display/index.ts
+++ redisinsight/ui/src/components/base/display/index.ts
@@ -7,3 +7,5 @@ import { Modal } from './modal'
 export { Loader, ProgressBarLoader, RiImage, RiLoadingLogo, Modal }
 
 export { RICollapsibleNavGroup } from './collapsible-nav-group/RICollapsibleNavGroup'
+
+export type { RICollapsibleNavGroupProps } from './collapsible-nav-group/RICollapsibleNavGroup'
