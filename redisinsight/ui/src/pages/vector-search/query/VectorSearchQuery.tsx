c1159d6f4560b1e3718ddf7378da5893c41de067
Krum Tyukenov <krum.tyukenov@redis.com>
2025-08-04 18:32:06 +0300

    RI-7196: add SavedQueriesScreen

--- redisinsight/ui/src/pages/vector-search/query/VectorSearchQuery.tsx
+++ redisinsight/ui/src/pages/vector-search/query/VectorSearchQuery.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { HeaderActions } from './HeaderActions'
-import { CreateIndexWrapper } from '../create-index/styles'
+import { VectorSearchScreenWrapper } from '../styles'
 
 // TODO: implement this component
 // https://www.figma.com/design/oO2eYRuuLmfzUYLkvCkFhM/Search-page?node-id=645-37412&t=TSwcttCYa4Ld9WzC-4
 export const VectorSearchQuery = () => (
-  <CreateIndexWrapper direction="column" justify="between">
+  <VectorSearchScreenWrapper direction="column" justify="between">
     <HeaderActions />
-  </CreateIndexWrapper>
+  </VectorSearchScreenWrapper>
 )
