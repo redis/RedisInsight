30560d7c1f3f221aaafdc2dcc131ae6272f1c2d6
Valentin Kirilov <valentin.kirilov@redis.com>
2025-08-05 16:25:05 +0300

    RI-7218 Add telemetry collection for Vector Search  (#4799)
    - extract the common logic for the page view telemetry events in a custom hook
    - add page view telemetry for the vector search page
    
    re #RI-7218

--- redisinsight/ui/src/pages/vector-search/VectorSearchPage.tsx
+++ redisinsight/ui/src/pages/vector-search/VectorSearchPage.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { TelemetryPageView } from 'uiSrc/telemetry'
+import { usePageViewTelemetry } from 'uiSrc/telemetry/usePageViewTelemetry'
 
 import { VectorSearchCreateIndex } from './create-index/VectorSearchCreateIndex'
 import { VectorSearchQuery } from './query/VectorSearchQuery'
 
 export const VectorSearchPage = () => {
   const hasIndexes = true
+
+  usePageViewTelemetry({
+    page: TelemetryPageView.VECTOR_SEARCH_PAGE,
+  })
+
+  // TODO: Set title, once we know the name of the page
+  // setTitle(
+  //   `${formatLongName(connectedInstanceName, 33, 0, '...')} ${getDbIndex(db)} - Vector Search`,
+  // )
 
   if (!hasIndexes) {
     return <VectorSearchCreateIndex />
