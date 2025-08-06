872be0190c1fdb59610c91141dd23c76b50bb98d
ArtemHoruzhenko <artsiom.kharuzhenka@gmail.com>
2025-08-06 11:48:04 +0300

    RI-7217 fix PR comments

--- redisinsight/ui/src/pages/rdi/pipeline-management/components/validation-errors-list/ValidationErrorsList.tsx
+++ redisinsight/ui/src/pages/rdi/pipeline-management/components/validation-errors-list/ValidationErrorsList.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Text } from 'uiSrc/components/base/text'
 
 export interface Props {
   validationErrors: string[]
@@ -8,19 +7,17 @@ export interface Props {
 const ValidationErrorsList = (props: Props) => {
   const { validationErrors } = props
 
-  return (
-    <>
-      {validationErrors?.length && (
-        <Text>
-          <ul>
-            {validationErrors.map((err, index) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <li key={index}>{err}</li>
-            ))}
-          </ul>
-        </Text>
-      )}
-    </>
+  if(!validationErrors?.length) {
+    return null
+  }
+
+  return  (
+    <ul>
+      {validationErrors.map((err, index) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <li key={index}>{err}</li>
+      ))}
+    </ul>
   )
 }
 
