8640a2b0c36a2b6499c856bc32f6dd8130ea4974
Krum Tyukenov <krum.tyukenov@redis.com>
2025-08-06 11:12:40 +0300

    update svg mock

--- redisinsight/__mocks__/svg.js
+++ redisinsight/__mocks__/svg.js
@@ -1,2 +1,11 @@
-export default 'SvgrURL';
-export const ReactComponent = 'div';
+import React from 'react';
+
+// Mock SVG component for Jest tests
+const SvgMock = React.forwardRef((props, ref) => (
+  <svg ref={ref} {...props} data-testid="svg-mock" />
+));
+
+SvgMock.displayName = 'SvgMock';
+
+export default SvgMock;
+export const ReactComponent = SvgMock;
