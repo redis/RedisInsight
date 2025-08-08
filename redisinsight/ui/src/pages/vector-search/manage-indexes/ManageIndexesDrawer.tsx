5cb47715a37dd6f0c8e7339acab4fdd7166f9873
Valentin Kirilov <valentin.kirilov@redis.com>
2025-08-08 09:33:03 +0300

    feat(ui): collect telemetry on vector search page when open/close manage indexes drawer
    re #RI-7218

--- redisinsight/ui/src/pages/vector-search/manage-indexes/ManageIndexesDrawer.tsx
+++ redisinsight/ui/src/pages/vector-search/manage-indexes/ManageIndexesDrawer.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { DrawerProps } from '@redis-ui/components'
+import { useParams } from 'react-router-dom'
 import {
   Drawer,
   DrawerBody,
   DrawerHeader,
 } from 'uiSrc/components/base/layout/drawer'
 import { ManageIndexesList } from './ManageIndexesList'
+import { sendEventTelemetry, TelemetryEvent } from 'uiSrc/telemetry'
 
 export interface ManageIndexesDrawerProps extends DrawerProps {}
 
@@ -13,16 +15,40 @@ export const ManageIndexesDrawer = ({
   open,
   onOpenChange,
   ...rest
-}: ManageIndexesDrawerProps) => (
-  <Drawer
-    open={open}
-    onOpenChange={onOpenChange}
-    data-testid="manage-indexes-drawer"
-    {...rest}
-  >
-    <DrawerHeader title="Manage indexes" />
-    <DrawerBody data-testid="manage-indexes-drawer-body">
-      <ManageIndexesList />
-    </DrawerBody>
-  </Drawer>
-)
+}: ManageIndexesDrawerProps) => {
+  const { instanceId } = useParams<{ instanceId: string }>()
+
+  const onDrawerDidOpen = () => {
+    sendEventTelemetry({
+      event: TelemetryEvent.SEARCH_MANAGE_INDEXES_DRAWER_OPENED,
+      eventData: {
+        databaseId: instanceId,
+      },
+    })
+  }
+
+  const onDrawerDidClose = () => {
+    sendEventTelemetry({
+      event: TelemetryEvent.SEARCH_MANAGE_INDEXES_DRAWER_CLOSED,
+      eventData: {
+        databaseId: instanceId,
+      },
+    })
+  }
+
+  return (
+    <Drawer
+      open={open}
+      onOpenChange={onOpenChange}
+      onDrawerDidOpen={onDrawerDidOpen}
+      onDrawerDidClose={onDrawerDidClose}
+      data-testid="manage-indexes-drawer"
+      {...rest}
+    >
+      <DrawerHeader title="Manage indexes" />
+      <DrawerBody data-testid="manage-indexes-drawer-body">
+        <ManageIndexesList />
+      </DrawerBody>
+    </Drawer>
+  )
+}
