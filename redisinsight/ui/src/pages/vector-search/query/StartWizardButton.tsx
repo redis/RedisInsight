e8ffdb7f0f006554d60b7b3fabe6542bf52220e4
Pavel Angelov <pavel.angelov@redis.com>
2025-08-12 15:52:18 +0300

    use this

--- redisinsight/ui/src/pages/vector-search/query/StartWizardButton.tsx
+++ redisinsight/ui/src/pages/vector-search/query/StartWizardButton.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import styled from 'styled-components'
 import { CallOut } from 'uiSrc/components/base/display/call-out/CallOut'
-import { Button } from 'uiSrc/components/base/forms/buttons'
-
-const StyledButton = styled(Button)`
-  margin-left: ${({ theme }) => theme.core.space.space150};
-`
 
 export const StartWizardButton = () => (
-  <CallOut variant="success">
+  <CallOut
+    variant="success"
+    actions={{
+      primary: {
+        label: 'Get started',
+        onClick: () => {},
+      },
+    }}
+  >
     Power fast, real-time semantic AI search with vector search.
-    <StyledButton variant="secondary-ghost" size="small">
-      Get started
-    </StyledButton>
   </CallOut>
 )
