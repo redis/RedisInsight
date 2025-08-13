25205d46145b60f5dbe395e4b0a034047da6a3de
Dijana Antovska <dijana.antovska@redis.com>
2025-08-13 12:14:17 +0300

    api telemetry: rename RE_CLOUD -> REDIS_CLOUD

--- redisinsight/api/src/modules/cloud/autodiscovery/cloud-autodiscovery.analytics.ts
+++ redisinsight/api/src/modules/cloud/autodiscovery/cloud-autodiscovery.analytics.ts
@@ -30,7 +30,7 @@ export class CloudAutodiscoveryAnalytics extends TelemetryBaseService {
     try {
       this.sendEvent(
         sessionMetadata,
-        TelemetryEvents.RECloudSubscriptionsDiscoverySucceed,
+        TelemetryEvents.RedisCloudSubscriptionsDiscoverySucceed,
         {
           numberOfActiveSubscriptions: subscriptions.filter(
             (sub) => sub.status === CloudSubscriptionStatus.Active,
@@ -53,7 +53,7 @@ export class CloudAutodiscoveryAnalytics extends TelemetryBaseService {
   ) {
     this.sendFailedEvent(
       sessionMetadata,
-      TelemetryEvents.RECloudSubscriptionsDiscoveryFailed,
+      TelemetryEvents.RedisCloudSubscriptionsDiscoveryFailed,
       exception,
       {
         type,
@@ -70,7 +70,7 @@ export class CloudAutodiscoveryAnalytics extends TelemetryBaseService {
     try {
       this.sendEvent(
         sessionMetadata,
-        TelemetryEvents.RECloudDatabasesDiscoverySucceed,
+        TelemetryEvents.RedisCloudDatabasesDiscoverySucceed,
         {
           numberOfActiveDatabases: databases.filter(
             (db) => db.status === CloudDatabaseStatus.Active,
@@ -97,7 +97,7 @@ export class CloudAutodiscoveryAnalytics extends TelemetryBaseService {
   ) {
     this.sendFailedEvent(
       sessionMetadata,
-      TelemetryEvents.RECloudDatabasesDiscoveryFailed,
+      TelemetryEvents.RedisCloudDatabasesDiscoveryFailed,
       exception,
       { authType },
     );
