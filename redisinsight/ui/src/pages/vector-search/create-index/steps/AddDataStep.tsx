4537a6e58f5bfd107bfc1c19e21be9f8cdf51ae5
Valentin Kirilov <valentin.kirilov@redis.com>
2025-08-20 09:07:07 +0300

    fix(ui): added support for dark theme for the "Data content" section
    - make the icons in the "Data content" section on the first step of the
      Create Index flow on the Vector Search page to support dark theme
    
    re #RI-7359

--- redisinsight/ui/src/pages/vector-search/create-index/steps/AddDataStep.tsx
+++ redisinsight/ui/src/pages/vector-search/create-index/steps/AddDataStep.tsx
@@ -9,47 +9,55 @@ import {
   SmallSelectionBox,
   StyledBoxSelectionGroup,
 } from './styles'
-import { indexDataContent, indexType, sampleDatasetOptions } from './config'
+import {
+  useGetIndexDataContent,
+  indexType,
+  sampleDatasetOptions,
+} from './config'
 import { IStepComponent, SampleDataType, StepComponentProps } from '../types'
 
 export const AddDataStep: IStepComponent = ({
   parameters,
   setParameters,
-}: StepComponentProps) => (
-  <>
-    <FlexItem direction="column" $gap="m">
-      <StyledBoxSelectionGroup defaultValue={parameters.searchIndexType}>
-        {indexType.map((type) => (
-          <LargeSelectionBox
-            box={type}
-            key={type.value}
-            onClick={() => setParameters({ searchIndexType: type.value })}
-          />
-        ))}
-      </StyledBoxSelectionGroup>
-    </FlexItem>
-    <FlexItem direction="column" $gap="m">
-      <Text size="L">Select sample dataset</Text>
-      <RiRadioGroup
-        items={sampleDatasetOptions}
-        layout="horizontal"
-        defaultValue={parameters.sampleDataType}
-        onChange={(id) =>
-          setParameters({ sampleDataType: id as SampleDataType })
-        }
-      />
-    </FlexItem>
-    <FlexItem direction="column" $gap="m">
-      <Text>Data content</Text>
-      <StyledBoxSelectionGroup defaultValue={parameters.dataContent}>
-        {indexDataContent.map((type) => (
-          <SmallSelectionBox
-            box={type}
-            key={type.value}
-            onClick={() => setParameters({ dataContent: type.value })}
-          />
-        ))}
-      </StyledBoxSelectionGroup>
-    </FlexItem>
-  </>
-)
+}: StepComponentProps) => {
+  const indexDataContent = useGetIndexDataContent()
+
+  return (
+    <>
+      <FlexItem direction="column" $gap="m">
+        <StyledBoxSelectionGroup defaultValue={parameters.searchIndexType}>
+          {indexType.map((type) => (
+            <LargeSelectionBox
+              box={type}
+              key={type.value}
+              onClick={() => setParameters({ searchIndexType: type.value })}
+            />
+          ))}
+        </StyledBoxSelectionGroup>
+      </FlexItem>
+      <FlexItem direction="column" $gap="m">
+        <Text size="L">Select sample dataset</Text>
+        <RiRadioGroup
+          items={sampleDatasetOptions}
+          layout="horizontal"
+          defaultValue={parameters.sampleDataType}
+          onChange={(id) =>
+            setParameters({ sampleDataType: id as SampleDataType })
+          }
+        />
+      </FlexItem>
+      <FlexItem direction="column" $gap="m">
+        <Text>Data content</Text>
+        <StyledBoxSelectionGroup defaultValue={parameters.dataContent}>
+          {indexDataContent.map((type) => (
+            <SmallSelectionBox
+              box={type}
+              key={type.value}
+              onClick={() => setParameters({ dataContent: type.value })}
+            />
+          ))}
+        </StyledBoxSelectionGroup>
+      </FlexItem>
+    </>
+  )
+}
