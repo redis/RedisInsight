864dad406a4610adde4ce6b7f5698a057f35c9b1
Dijana Antovska <dijana.antovska@redis.com>
2025-08-25 01:23:12 +0300

    refactor recommendation copy component

--- redisinsight/ui/src/components/recommendation/recommendation-copy-component/RecommendationCopyComponent.tsx
+++ redisinsight/ui/src/components/recommendation/recommendation-copy-component/RecommendationCopyComponent.tsx
@@ -1,14 +1,46 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import cx from 'classnames'
+import styled from 'styled-components'
 
 import { bufferToString } from 'uiSrc/utils'
 import { sendEventTelemetry, TelemetryEvent } from 'uiSrc/telemetry'
 
-import { Text, ColorText } from 'uiSrc/components/base/text'
+import { Text } from 'uiSrc/components/base/text'
 import { IconButton } from 'uiSrc/components/base/forms/buttons'
 import { CopyIcon } from 'uiSrc/components/base/icons'
-import styles from './styles.module.scss'
+import { FlexGroup } from 'uiSrc/components/base/layout/flex'
+
+const StyledWrapper = styled.div`
+  margin-top: 15px;
+`
+
+const StyledKeyNameWrapper = styled(FlexGroup)<{ $isDbAnalysis: boolean }>`
+  margin-top: 5px;
+  border: 1px dashed ${({ theme }) => theme.semantic.color.border.neutral500};
+  border-radius: 4px;
+  background-color: ${(props) =>
+    props.$isDbAnalysis
+      ? props.theme.semantic.color.background.neutral100
+      : props.theme.semantic.color.background.neutral400};
+`
+
+const StyledKeyName = styled(Text)`
+  padding: 4px;
+  font:
+    normal normal normal 13px/16px Graphik,
+    sans-serif !important;
+  height: 26px;
+`
+
+const StyledText = styled(Text)`
+  font:
+    normal normal normal 13px/16px Graphik,
+    sans-serif !important;
+`
+
+const StyledIconButton = styled(IconButton)`
+  margin-right: 5px;
+`
 
 export interface IProps {
   keyName: string
@@ -42,29 +74,23 @@ const RecommendationCopyComponent = ({
   }
 
   return (
-    <div className={styles.wrapper}>
-      <Text className={styles.text}>
-        Example of a key that may be relevant:
-      </Text>
-      <div className={styles.keyNameWrapper}>
-        <Text
-          className={cx(styles.keyName, 'truncateText', {
-            [styles.dbAnalysis]: !live,
-          })}
-          component="div"
+    <StyledWrapper>
+      <StyledText>Example of a key that may be relevant:</StyledText>
+      <StyledKeyNameWrapper align="center" $isDbAnalysis={!live}>
+        <StyledKeyName
+          className="truncateText"
           data-testid="recommendation-key-name"
         >
           {formattedName}
-        </Text>
-        <IconButton
+        </StyledKeyName>
+        <StyledIconButton
           onClick={handleCopy}
-          className={styles.btn}
           icon={CopyIcon}
           data-testid="copy-key-name-btn"
           aria-label="copy key name"
         />
-      </div>
-    </div>
+      </StyledKeyNameWrapper>
+    </StyledWrapper>
   )
 }
 
