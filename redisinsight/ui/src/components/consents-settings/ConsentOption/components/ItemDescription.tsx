e59c7dafb4855e9c3f9c000565fa9d648366959e
Dijana Antovska <dijana.antovska@redis.com>
2025-08-27 11:11:24 +0300

    refactor privacy policy link

--- redisinsight/ui/src/components/consents-settings/ConsentOption/components/ItemDescription.tsx
+++ redisinsight/ui/src/components/consents-settings/ConsentOption/components/ItemDescription.tsx
@@ -1,6 +1,8 @@
 import parse from 'html-react-parser'
 import React from 'react'
 import { Link } from 'uiSrc/components/base/link/Link'
+import { EXTERNAL_LINKS, UTM_MEDIUMS } from 'uiSrc/constants/links'
+import { getUtmExternalLink } from 'uiSrc/utils/links'
 
 interface ItemDescriptionProps {
   description: string
@@ -18,7 +20,10 @@ export const ItemDescription = ({
         <Link
           color="primary"
           target="_blank"
-          href="https://redis.io/legal/privacy-policy/?utm_source=redisinsight&utm_medium=app&utm_campaign=telemetry"
+          href={getUtmExternalLink(EXTERNAL_LINKS.legalPrivacyPolicy, {
+            medium: UTM_MEDIUMS.App,
+            campaign: 'telemetry',
+          })}
         >
           Privacy Policy
         </Link>
