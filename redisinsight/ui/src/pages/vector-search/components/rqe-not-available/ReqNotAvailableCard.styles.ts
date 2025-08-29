fc35aa5b3d0e31a37f8dc20a3c26da494f8b06e0
Valentin Kirilov <valentin.kirilov@redis.com>
2025-08-29 13:42:42 +0300

    feat(ui): rework the "Redis Query Engine" not available for Vector Search
    - make sure we reuse the already available component, instead of defining it once again
    - update texts
    
    re #RI-7363

--- redisinsight/ui/src/pages/vector-search/components/rqe-not-available/ReqNotAvailableCard.styles.ts
+++ redisinsight/ui/src/pages/vector-search/components/rqe-not-available/ReqNotAvailableCard.styles.ts
@@ -2,28 +2,16 @@ import styled from 'styled-components'
 import { Card } from 'uiSrc/components/base/layout'
 import { FlexGroup } from 'uiSrc/components/base/layout/flex'
 
-export const StyledCard = styled(Card)<{ backgroundImage: string }>`
+export const StyledCard = styled(Card)`
   height: 100%;
   justify-content: center;
   align-items: center;
   position: relative;
-  background-image: ${({ backgroundImage }) =>
-    backgroundImage ? `url(${backgroundImage})` : 'none'};
-
-  background-repeat: no-repeat;
-  background-position: left ${({ theme }) => theme.core.space.space150} bottom
-    ${({ theme }) => theme.core.space.space150};
-  background-size: auto 238px;
 `
 
 export const StyledCardBody = styled(FlexGroup)`
   flex: 0 0 auto;
-  max-width: 520px;
+  max-width: 600px;
   height: fit-content;
   align-items: center;
-`
-
-export const StyledCardDescription = styled(FlexGroup)`
-  align-items: center;
-  text-align: center;
 `
