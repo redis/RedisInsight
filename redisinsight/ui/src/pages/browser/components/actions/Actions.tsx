2a3ed1adf686f7e22ca9749db5c5dbdc60d90838
Dijana Antovska <dijana.antovska@redis.com>
2025-09-02 17:25:04 +0300

    remove bulk actions icon, replace with subscription icon as in the designs

--- redisinsight/ui/src/pages/browser/components/actions/Actions.tsx
+++ redisinsight/ui/src/pages/browser/components/actions/Actions.tsx
@@ -12,7 +12,7 @@ import {
 import styles from 'uiSrc/pages/browser/components/browser-search-panel/styles.module.scss'
 import { setBulkActionType } from 'uiSrc/slices/browser/bulkActions'
 import { BulkActionsType, FeatureFlags } from 'uiSrc/constants'
-import { BulkActionsIcon } from 'uiSrc/components/base/icons'
+import { SubscriptionsIcon } from 'uiSrc/components/base/icons'
 import { FeatureFlagComponent } from 'uiSrc/components'
 import { connectedInstanceSelector } from 'uiSrc/slices/instances/instances'
 import { keysSelector } from 'uiSrc/slices/browser/keys'
@@ -56,7 +56,7 @@ const Actions = ({ handleAddKeyPanel, handleBulkActionsPanel }: Props) => {
   const BulkActionsBtn = (
     <SecondaryButton
       color="secondary"
-      icon={BulkActionsIcon}
+      icon={SubscriptionsIcon}
       onClick={openBulkActions}
       data-testid="btn-bulk-actions"
       aria-label="bulk actions"
