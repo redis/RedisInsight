1eb268a44931cdaef07d1782d528e0a1e7a16b51
Valentin Kirilov <valentin.kirilov@redis.com>
2025-09-04 14:05:34 +0300

    test(e2e): added tests for the vector search page
    - it should open the page when all reqreumenets are met
    - it should open fallback message when redis quirey engine modue is not enabled
    - it should show fallback message when redis version doens't support vector sets
    
    re #RI-7275

--- tests/playwright/pageObjects/pages/vector-search/vector-search-page.ts
+++ tests/playwright/pageObjects/pages/vector-search/vector-search-page.ts
@@ -11,9 +11,19 @@ export class VectorSearchPage extends BasePage {
     // SELECTORS
     public readonly vectorSearchPage: Locator
     public readonly searchTab: Locator
+    public readonly cloudLoginModal: Locator
+
+    // VECTOR SET NOT AVAILABLE BANNER
+    public readonly vectorSetNotAvailableBanner: Locator
+    public readonly freeRedisCloudDatabaseButton: Locator
+
+    // RQE NOT AVAILABLE CARD
+    public readonly rqeNotAvailableCard: Locator
+    public readonly createRedisCloudDatabaseButton: Locator
 
     // BUTTONS
     public readonly getStartedButton: Locator
+    public readonly startWizardButton: Locator
 
     constructor(page: Page) {
         super(page)
@@ -25,11 +35,31 @@ export class VectorSearchPage extends BasePage {
         // CONTAINERS
         this.vectorSearchPage = page.getByTestId('vector-search-page')
         this.searchTab = page.getByRole('tab', { name: 'Search' })
+        this.cloudLoginModal = page.getByTestId('social-oauth-dialog')
+
+        // VECTOR SET NOT AVAILABLE BANNER
+        this.vectorSetNotAvailableBanner = page.getByTestId(
+            'vector-set-not-available-banner',
+        )
+        this.freeRedisCloudDatabaseButton =
+            this.vectorSetNotAvailableBanner.getByRole('button', {
+                name: 'Free Redis Cloud DB',
+            })
+
+        // RQE NOT AVAILABLE CARD
+        this.rqeNotAvailableCard = page.getByTestId(
+            'vector-search-page--rqe-not-available',
+        )
+        this.createRedisCloudDatabaseButton =
+            this.rqeNotAvailableCard.getByRole('button', {
+                name: 'Get Started For Free',
+            })
 
         // BUTTONS
         this.getStartedButton = page.getByRole('button', {
             name: 'Get started',
         })
+        this.startWizardButton = page.getByTestId('start-wizard-button')
     }
 
     async navigateToVectorSearchPage(): Promise<void> {
