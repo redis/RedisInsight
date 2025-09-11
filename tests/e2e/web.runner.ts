e9d8d992db7fa7ccce00f90d1a363ca95b1e567e
ArtemHoruzhenko <artsiom.kharuzhenka@gmail.com>
2025-09-11 09:54:38 +0300

    RI-7131: fix electron tests ts errors

--- tests/e2e/web.runner.ts
+++ tests/e2e/web.runner.ts
@@ -10,8 +10,7 @@ import testcafe from 'testcafe';
                         configPath: 'tsconfig.testcafe.json',
                         experimentalDecorators: true
                     } })
-                // .src((process.env.TEST_FILES || 'tests/web/**/*.e2e.ts').split('\n'))
-                .src((process.env.TEST_FILES || 'tests/electron/**/*.e2e.ts').split('\n'))
+                .src((process.env.TEST_FILES || 'tests/web/**/*.e2e.ts').split('\n'))
                 .browsers([`${process.env.TEST_BROWSER || 'chromium'} --disable-search-engine-choice-screen --ignore-certificate-errors --disable-dev-shm-usage --no-sandbox`])
                 .screenshots({
                     path: 'report/screenshots/',
