4c3857b4b02a8cb1a1613b4284d52efefae2cb73
ArtemHoruzhenko <artsiom.kharuzhenka@gmail.com>
2025-10-22 16:33:25 +0300

    RI-7670: fix tls and re ITests

--- redisinsight/api/test/api/database/PATCH-databases-id.test.ts
+++ redisinsight/api/test/api/database/PATCH-databases-id.test.ts
@@ -669,7 +669,7 @@ describe(`PATCH /databases/:id`, () => {
 
         await validateApiCall({
           endpoint: () => endpoint(constants.TEST_INSTANCE_ID_2),
-          statusCode: 400,
+          statusCode: 424,
           data: {
             name: dbName,
             host: constants.TEST_REDIS_HOST,
@@ -679,10 +679,9 @@ describe(`PATCH /databases/:id`, () => {
             caCert: null,
           },
           responseBody: {
-            statusCode: 400,
-            // todo: verify error handling because right now messages are different
-            // message: 'Could not connect to',
-            error: 'Bad Request',
+            statusCode: 424,
+            message: 'Could not connect to redis:6379, please check the CA or Client certificate.',
+            error: 'RedisConnectionIncorrectCertificateException',
           },
         });
 
@@ -695,7 +694,7 @@ describe(`PATCH /databases/:id`, () => {
 
         await validateApiCall({
           endpoint: () => endpoint(constants.TEST_INSTANCE_ID_2),
-          statusCode: 400,
+          statusCode: 424,
           data: {
             name: dbName,
             host: constants.TEST_REDIS_HOST,
@@ -708,10 +707,9 @@ describe(`PATCH /databases/:id`, () => {
             },
           },
           responseBody: {
-            statusCode: 400,
-            // todo: verify error handling because right now messages are different
-            // message: 'Could not connect to',
-            error: 'Bad Request',
+            statusCode: 424,
+            message: 'Could not connect to redis:6379, please check the CA or Client certificate.',
+            error: 'RedisConnectionIncorrectCertificateException',
           },
         });
 
