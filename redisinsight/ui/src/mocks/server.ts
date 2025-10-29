e51d437ad4f163918cf31af92c8d1e38f9a6d22f
ArtemHoruzhenko <artsiom.kharuzhenka@gmail.com>
2025-10-29 15:39:38 +0200

    Properly msw server to return success response for unhandled requests temporary Fix store issue in RediSearchIndexesList.spec.tsx

--- redisinsight/ui/src/mocks/server.ts
+++ redisinsight/ui/src/mocks/server.ts
@@ -1,5 +1,14 @@
 import { setupServer } from 'msw/node'
+import { http, HttpResponse } from 'msw'
 import { handlers } from './handlers'
 
 // Setup requests interception using the given handlers.
-export const mswServer = setupServer(...handlers)
+export const mswServer = setupServer(
+  ...handlers,
+  http.all(
+    '*',
+    jest
+      .fn()
+      .mockImplementation(async () => HttpResponse.json({}, { status: 200 })),
+  ),
+)
