db7066a58dba4c4611ffc2ca78963a2041a3ed65
Dijana Antovska <dijana.antovska@redis.com>
2025-11-03 17:22:41 +0200

    remove not needed important

--- redisinsight/ui/src/components/form-dialog/FormDialog.styles.ts
+++ redisinsight/ui/src/components/form-dialog/FormDialog.styles.ts
@@ -2,11 +2,11 @@ import styled from 'styled-components'
 import { Modal } from 'uiSrc/components/base/display/modal'
 
 export const StyledFormDialogContent = styled(Modal.Content.Compose)`
-  width: 900px !important;
-  height: 700px !important;
+  width: 900px;
+  height: 700px;
 
-  max-width: calc(100vw - 120px) !important;
-  max-height: calc(100vh - 120px) !important;
+  max-width: calc(100vw - 120px);
+  max-height: calc(100vh - 120px);
 `
 
 export const StyledFormDialogContentBody = styled(Modal.Content.Body)`
