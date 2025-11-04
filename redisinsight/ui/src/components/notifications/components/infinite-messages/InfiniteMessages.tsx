fe1ffff60b4070ea05ec00b0e26ef8edcaaa87d3
Valentin Kirilov <valentin.kirilov@redis.com>
2025-11-04 14:17:20 +0200

    refactor(ui): rework external link component
    - change the display mode to inline-flex, so it does alignproperly when rendered with icon
    
    re #RI-7415

--- redisinsight/ui/src/components/notifications/components/infinite-messages/InfiniteMessages.tsx
+++ redisinsight/ui/src/components/notifications/components/infinite-messages/InfiniteMessages.tsx
@@ -165,12 +165,7 @@ export const INFINITE_MESSAGES: InfiniteMessagesType = {
           <Spacer size="m" />
           <Row justify="between" align="center">
             <FlexItem>
-              <ExternalLink
-                href={MANAGE_DB_LINK}
-                iconSize="S"
-                variant="inline"
-                style={{ display: 'inline-flex' }}
-              >
+              <ExternalLink href={MANAGE_DB_LINK} iconSize="S" variant="inline">
                 Manage DB
               </ExternalLink>
             </FlexItem>
