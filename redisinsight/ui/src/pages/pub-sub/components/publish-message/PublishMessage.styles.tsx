bd947a74d656527adb4c22e6cd56c291b11b9acc
Pavel Angelov <pavel.angelov@redis.com>
2025-11-10 15:40:52 +0200

    remove leftover file

--- redisinsight/ui/src/pages/pub-sub/components/publish-message/PublishMessage.styles.tsx
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components'
-import { Row } from 'uiSrc/components/base/layout/flex'
-
-export const StyledWrapper = styled(Row)`
-  /* border-top: 1px solid ${({ theme }) => theme.semantic.color.border.neutral500}; */
-`
