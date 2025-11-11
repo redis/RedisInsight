adde36c73ac5c808ce91e82a433b9a2bfe752a73
ArtemHoruzhenko <artsiom.kharuzhenka@gmail.com>
2025-11-11 14:40:42 +0200

    RI-7687: fix double scroll issue on the RDI statistics page

--- redisinsight/ui/src/templates/rdi-instance-page-template/styles.ts
+++ redisinsight/ui/src/templates/rdi-instance-page-template/styles.ts
@@ -2,5 +2,5 @@ import styled from 'styled-components'
 import { FlexItem } from 'uiSrc/components/base/layout/flex'
 
 export const RdiInstancePageContentContainer = styled(FlexItem)`
-  height: calc(100% - 230px);
+  height: calc(100% - 210px);
 `
