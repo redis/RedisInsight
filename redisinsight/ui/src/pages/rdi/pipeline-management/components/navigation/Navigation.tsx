cb88d45ad15b8b955a3ac96714856c8373f1d866
Pavel Angelov <pavel.angelov@redis.com>
2025-11-16 21:39:56 +0200

    add loading indicator

--- redisinsight/ui/src/pages/rdi/pipeline-management/components/navigation/Navigation.tsx
+++ redisinsight/ui/src/pages/rdi/pipeline-management/components/navigation/Navigation.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 import { useHistory, useLocation, useParams } from 'react-router-dom'
 
 import { Title } from 'uiSrc/components/base/text'
 import { PageNames, Pages } from 'uiSrc/constants'
 import { RdiPipelineTabs } from 'uiSrc/slices/interfaces/rdi'
+import { rdiPipelineSelector } from 'uiSrc/slices/rdi/pipeline'
 import { Nullable } from 'uiSrc/utils'
 
 import { NavigationContainer } from './Navigation.styles'
 import { ConfigurationCard, JobsCard } from './cards'
+import { Loader } from 'uiSrc/components/base/display'
 
 const getSelectedTab = (path: string, rdiInstanceId: string) => {
   const tabsPath = path?.replace(
@@ -30,6 +33,7 @@ const Navigation = () => {
   const history = useHistory()
   const { pathname } = useLocation()
   const { rdiInstanceId } = useParams<{ rdiInstanceId: string }>()
+  const { loading } = useSelector(rdiPipelineSelector)
 
   const onSelectedTabChanged = (id: string | RdiPipelineTabs) => {
     if (id === RdiPipelineTabs.Config) {
@@ -51,15 +55,21 @@ const Navigation = () => {
         Pipeline management
       </Title>
 
-      <ConfigurationCard
-        onSelect={onSelectedTabChanged}
-        isSelected={selectedTab === RdiPipelineTabs.Config}
-      />
+      {loading && <Loader size="xl" />}
 
-      <JobsCard
-        onSelect={onSelectedTabChanged}
-        isSelected={selectedTab === RdiPipelineTabs.Jobs}
-      />
+      {!loading && (
+        <>
+          <ConfigurationCard
+            onSelect={onSelectedTabChanged}
+            isSelected={selectedTab === RdiPipelineTabs.Config}
+          />
+
+          <JobsCard
+            onSelect={onSelectedTabChanged}
+            isSelected={selectedTab === RdiPipelineTabs.Jobs}
+          />
+        </>
+      )}
     </NavigationContainer>
   )
 }
