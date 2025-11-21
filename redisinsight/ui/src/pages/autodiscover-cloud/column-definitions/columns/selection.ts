ac1bd7c23319ee17066ffaffa485de7f9ade2778
pd-redis <petar.dzhambazov@redis.com>
2025-11-21 18:59:42 +0200

    remove unused files

--- redisinsight/ui/src/pages/autodiscover-cloud/column-definitions/columns/selection.ts
+++
@@ -1,6 +0,0 @@
-import { type RedisCloudSubscription } from 'uiSrc/slices/interfaces'
-import { getSelectionColumn } from 'uiSrc/pages/autodiscover-cloud/utils'
-
-export const selectionColumn = () => {
-  return getSelectionColumn<RedisCloudSubscription>()
-}
