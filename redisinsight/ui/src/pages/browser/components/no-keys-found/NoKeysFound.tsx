3033d8a49f1c15afa75c11386f44874df9d35a87
Valentin Kirilov <valentin.kirilov@redis.com>
2025-11-25 11:33:32 +0200

    refactor(ui): remove deprecated sass styles
    re RED-177178

--- redisinsight/ui/src/pages/browser/components/no-keys-found/NoKeysFound.tsx
+++ redisinsight/ui/src/pages/browser/components/no-keys-found/NoKeysFound.tsx
@@ -20,12 +20,9 @@ import { TutorialsIds } from 'uiSrc/constants'
 
 import { Spacer } from 'uiSrc/components/base/layout/spacer'
 import { Title } from 'uiSrc/components/base/text/Title'
-import { RiImage } from 'uiSrc/components/base/display'
-import { Row } from 'uiSrc/components/base/layout/flex'
+import { Col, Row } from 'uiSrc/components/base/layout/flex'
 import LoadSampleData from '../load-sample-data'
-import { AddKeysManuallyButton } from './NoKeysFound.styles'
-
-import styles from './styles.module.scss'
+import { AddKeysManuallyButton, StyledImage } from './NoKeysFound.styles'
 
 export interface Props {
   onAddKeyPanel: (value: boolean) => void
@@ -61,10 +58,10 @@ const NoKeysFound = (props: Props) => {
   }
 
   return (
-    <div className={styles.container} data-testid="no-result-found-msg">
-      <RiImage className={styles.img} src={NoDataImg} alt="no results" />
+    <Col align="center" data-testid="no-result-found-msg">
+      <StyledImage src={NoDataImg} alt="no results" />
       <Spacer />
-      <Title color="primary" className={styles.title} size="S">
+      <Title color="primary" size="XL">
         Let&apos;s start working
       </Title>
       <Spacer />
@@ -77,7 +74,7 @@ const NoKeysFound = (props: Props) => {
           + Add key manually
         </AddKeysManuallyButton>
       </Row>
-    </div>
+    </Col>
   )
 }
 
