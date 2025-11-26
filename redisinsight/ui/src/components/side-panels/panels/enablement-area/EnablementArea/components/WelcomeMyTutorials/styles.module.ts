6c8a31d3f3dd678377ce1b63b17f76daf93f7349
Dijana Antovska <dijana.antovska@redis.com>
2025-11-26 09:12:28 +0200

    reafactor welcome tutorials card component

---
+++ redisinsight/ui/src/components/side-panels/panels/enablement-area/EnablementArea/components/WelcomeMyTutorials/styles.module.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+import { Row } from 'uiSrc/components/base/layout/flex'
+
+export const StyledRow = styled(Row)`
+  padding: ${({ theme }) => theme.core.space.space150};
+`
