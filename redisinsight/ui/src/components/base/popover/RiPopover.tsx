be37f5d46990ba9408cc200770441921ad49f806
ArtemHoruzhenko <artsiom.kharuzhenka@gmail.com>
2025-11-27 12:09:59 +0200

    RI-7797: move checks to component prop

--- redisinsight/ui/src/components/base/popover/RiPopover.tsx
+++ redisinsight/ui/src/components/base/popover/RiPopover.tsx
@@ -20,42 +20,39 @@ export const RiPopover = ({
   persistent,
   customOutsideDetector,
   ...props
-}: RiPopoverProps) => {
-  const popoverContent =
-    children && customOutsideDetector ? (
-      <OutsideClickDetector
-        onOutsideClick={(event) => closePopover?.(event as any)}
-      >
-        {children as JSX.Element}
-      </OutsideClickDetector>
-    ) : (
-      children
-    )
-
-  return (
-    <Popover
-      {...props}
-      open={isOpen}
-      onClickOutside={customOutsideDetector ? undefined : closePopover}
-      onKeyDown={(event) => {
-        // Close on escape press
-        if (event.key === keys.ESCAPE) {
-          closePopover?.(event as any)
-        }
-      }}
-      persistent={persistent}
-      content={popoverContent}
-      // Props passed to the children wrapper:
-      className={panelClassName}
-      maxWidth={maxWidth}
-      style={{
-        padding: panelPaddingSize && panelPaddingSizeMap[panelPaddingSize],
-      }}
-      autoFocus={ownFocus}
-      placement={anchorPosition && anchorPositionMap[anchorPosition]?.placement}
-      align={anchorPosition && anchorPositionMap[anchorPosition]?.align}
-    >
-      <span className={anchorClassName}>{button}</span>
-    </Popover>
-  )
-}
+}: RiPopoverProps) => (
+  <Popover
+    {...props}
+    open={isOpen}
+    onClickOutside={customOutsideDetector ? undefined : closePopover}
+    onKeyDown={(event) => {
+      // Close on escape press
+      if (event.key === keys.ESCAPE) {
+        closePopover?.(event as any)
+      }
+    }}
+    persistent={persistent}
+    content={
+      children && customOutsideDetector ? (
+        <OutsideClickDetector
+          onOutsideClick={(event) => closePopover?.(event as any)}
+        >
+          {children as JSX.Element}
+        </OutsideClickDetector>
+      ) : (
+        children
+      )
+    }
+    // Props passed to the children wrapper:
+    className={panelClassName}
+    maxWidth={maxWidth}
+    style={{
+      padding: panelPaddingSize && panelPaddingSizeMap[panelPaddingSize],
+    }}
+    autoFocus={ownFocus}
+    placement={anchorPosition && anchorPositionMap[anchorPosition]?.placement}
+    align={anchorPosition && anchorPositionMap[anchorPosition]?.align}
+  >
+    <span className={anchorClassName}>{button}</span>
+  </Popover>
+)
