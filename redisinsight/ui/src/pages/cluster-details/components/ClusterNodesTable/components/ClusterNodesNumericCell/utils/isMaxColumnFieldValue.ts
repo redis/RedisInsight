4dd1331b4095fe3a9e67a28bd4a2a0c4c245b6dd
Krum Tyukenov <krum.tyukenov@redis.com>
2025-11-28 13:18:41 +0200

    RI-7786: simplify isMaxColumnFieldValue

--- redisinsight/ui/src/pages/cluster-details/components/ClusterNodesTable/components/ClusterNodesNumericCell/utils/isMaxColumnFieldValue.ts
+++ redisinsight/ui/src/pages/cluster-details/components/ClusterNodesTable/components/ClusterNodesNumericCell/utils/isMaxColumnFieldValue.ts
@@ -4,25 +4,11 @@ export const isMaxColumnFieldValue = <T>(
   data: T[],
 ): boolean => {
   const numericValues = data
-    .map((node) => node[field] ?? 0)
+    .map((node) => node[field])
     .filter((v) => typeof v === 'number')
 
-  if (numericValues.length === 0) {
-    return false
-  }
-
-  const { max, count } = numericValues.reduce(
-    (prev, cur) => {
-      if (cur > prev.max) {
-        return { max: cur, count: 1 }
-      }
-      if (cur === prev.max) {
-        return { ...prev, count: prev.count + 1 }
-      }
-      return prev
-    },
-    { max: numericValues[0], count: 0 },
+  return (
+    Math.max(...numericValues) === value &&
+    numericValues.filter((v) => v === value).length === 1
   )
-
-  return value === max && count === 1
 }
