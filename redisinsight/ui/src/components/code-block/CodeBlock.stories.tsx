38c183015a594b191d73bfd650d31744a3618599
pd-redis <petar.dzhambazov@redis.com>
2025-12-02 14:41:00 +0200

    reformat

--- redisinsight/ui/src/components/code-block/CodeBlock.stories.tsx
+++ redisinsight/ui/src/components/code-block/CodeBlock.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-vite'
 
-import CodeBlock from "./index"
+import CodeBlock from './index'
 
 const codeBlockMeta: Meta<typeof CodeBlock> = {
   component: CodeBlock,
@@ -14,11 +14,11 @@ export const Default: Story = {
   args: {
     children: 'console.log("Hello, World!");',
   },
-};
+}
 
 export const WithCopyButton: Story = {
   args: {
     children: 'redis-cli --cluster create 127.0.0.1:7000 127.0.0.1:7001',
     isCopyable: true,
   },
-};
+}
