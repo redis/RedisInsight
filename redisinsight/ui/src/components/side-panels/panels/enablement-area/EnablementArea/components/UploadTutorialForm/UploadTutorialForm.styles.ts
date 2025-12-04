3871133de81359e42746e9d4265361a75157084a
Dijana Antovska <dijana.antovska@redis.com>
2025-12-04 10:01:07 +0200

    fix(ui): darken form border to match accordion border

--- redisinsight/ui/src/components/side-panels/panels/enablement-area/EnablementArea/components/UploadTutorialForm/UploadTutorialForm.styles.ts
+++ redisinsight/ui/src/components/side-panels/panels/enablement-area/EnablementArea/components/UploadTutorialForm/UploadTutorialForm.styles.ts
@@ -4,7 +4,7 @@ import { Theme } from 'uiSrc/components/base/theme/types'
 
 export const Wrapper = styled.div<React.HTMLAttributes<HTMLDivElement>>`
   border: 1px solid
-    ${({ theme }: { theme: Theme }) => theme.semantic.color.border.neutral500};
+    ${({ theme }: { theme: Theme }) => theme.semantic.color.border.neutral600};
   border-radius: ${({ theme }: { theme: Theme }) =>
     theme.components.card.borderRadius};
   padding: ${({ theme }: { theme: Theme }) => theme.core.space.space200};
