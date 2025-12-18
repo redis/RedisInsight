af1c9792e9e1a5221c615cb2ad0bc41c7288a724
Pavel Angelov <pavel.angelov@redis.com>
2025-12-18 16:04:48 +0200

    format eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,12 @@ module.exports = {
     node: true,
     browser: true,
   },
-  extends: ['airbnb-typescript', 'prettier', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'airbnb-typescript',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+  ],
   plugins: ['@typescript-eslint', 'import', 'prettier'],
   parser: '@typescript-eslint/parser',
   rules: {
