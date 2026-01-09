2684c220c093b6331679ee582992271e07e76b7d
Pavel Angelov <pavel.angelov@redis.com>
2026-01-09 09:53:12 +0200

    enable the rule for tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -313,7 +313,6 @@ module.exports = {
         'prefer-template': 'off',
         'import/order': 'off',
         '@typescript-eslint/no-use-before-define': 'off',
-        '@typescript-eslint/no-unused-vars': 'off',
         '@typescript-eslint/no-shadow': 'off',
         '@typescript-eslint/no-unused-expressions': 'off',
         '@typescript-eslint/naming-convention': 'off',
