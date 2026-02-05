3b515502b3568db1b26d1633756ac9d7cc8605ce
pd-redis <petar.dzhambazov@redis.com>
2026-02-05 13:38:19 +0200

    fix(ui): correct TypeScript types for styled components and VirtualGrid
    Updated styled component type definitions to use ComponentPropsWithRef for proper prop support. Fixed VirtualGrid to use theme hook instead of theme prop and corrected TypeScript signatures for callback functions and utilities.

--- redisinsight/ui/src/components/virtual-grid/VirtualGrid.styles.ts
+++ redisinsight/ui/src/components/virtual-grid/VirtualGrid.styles.ts
@@ -1,8 +1,27 @@
 import styled from 'styled-components'
 import { ComponentPropsWithRef } from 'react'
-import { type Theme } from 'uiSrc/components/base/theme/types'
+import { useTheme } from '@redis-ui/styles'
 
-const PADDING_CELL = '12px'
+const useGridStyles = () => {
+  const theme = useTheme()
+  const tableStyles = theme.components.table.table
+
+  return {
+    bg: tableStyles.tableContainer.backgroundColor,
+    borderColor: tableStyles.tableHeaderRow.separator.color,
+    borderRadius: tableStyles.tableContainer.borderRadius,
+    boxShadow: tableStyles.tableContainer.boxShadow,
+    headerBg: tableStyles.tableHeaderRow.bgColor,
+    headerHeight: tableStyles.tableHeaderRow.minHeight,
+    thCellColor: tableStyles.tableHeaderCell.color,
+    thCellPadding: tableStyles.tableHeaderCell.padding,
+    thCellBorder: tableStyles.tableHeaderCell.separator.width,
+    thCellBorderColor: tableStyles.tableHeaderCell.separator.color,
+    trStripedBgColor: tableStyles.tableBodyRow.backgroundColor,
+    tdColor: tableStyles.tableBodyCell.color,
+    tdPadding: tableStyles.tableBodyCell.padding,
+  }
+}
 
 export const Container = styled.div<ComponentPropsWithRef<'div'>>`
   position: relative;
@@ -11,6 +30,7 @@ export const Container = styled.div<ComponentPropsWithRef<'div'>>`
 `
 
 export const Grid = styled.div<ComponentPropsWithRef<'div'>>`
+  background-color: ${() => useGridStyles().bg};
   scrollbar-width: thin;
   position: relative;
 `
@@ -19,17 +39,14 @@ export const GridItem = styled.div<
   ComponentPropsWithRef<'div'> & { $odd?: boolean }
 >`
   z-index: 1;
-  padding: ${PADDING_CELL};
-  border: 1px solid
-    ${({ theme }: { theme: Theme }) => theme.semantic.color.border.secondary700};
+  padding: ${() => useGridStyles().tdPadding};
+  border: 1px solid ${() => useGridStyles().borderColor};
   border-left-width: 0;
   border-top-width: 0;
   cursor: pointer;
 
-  background-color: ${({ $odd, theme }: { $odd?: boolean; theme: Theme }) =>
-    $odd
-      ? theme.semantic.color.background.secondary800
-      : theme.semantic.color.background.secondary900};
+  background-color: ${({ $odd }: { $odd?: boolean }) =>
+    $odd ? 'inherit' : useGridStyles().trStripedBgColor};
 
   &.penult {
     border-right: 0;
@@ -40,22 +57,18 @@ export const GridItemLast = styled(GridItem)`
   z-index: 6;
   margin-top: -50px;
   padding-top: 16px;
-  border-left: 1px solid
-    ${({ theme }: { theme: Theme }) => theme.semantic.color.border.secondary700};
 `
 
 export const GridHeaderCell = styled.hgroup<ComponentPropsWithRef<'hgroup'>>`
-  border: 1px solid
-    ${({ theme }: { theme: Theme }) => theme.semantic.color.border.secondary700};
+  border: 1px solid ${() => useGridStyles().thCellBorderColor};
+  height: ${() => useGridStyles().headerHeight};
 
   &:not(:last-of-type) {
     border-right: 0;
   }
 
   &:first-of-type {
-    border-right: 1px solid
-      ${({ theme }: { theme: Theme }) =>
-        theme.semantic.color.border.secondary700};
+    border-right: 1px solid ${() => useGridStyles().thCellBorderColor};
   }
 
   &:last-of-type,
@@ -64,9 +77,7 @@ export const GridHeaderCell = styled.hgroup<ComponentPropsWithRef<'hgroup'>>`
   }
 
   &:last-of-type {
-    border-left: 1px solid
-      ${({ theme }: { theme: Theme }) =>
-        theme.semantic.color.border.secondary700};
+    border-left: 1px solid ${() => useGridStyles().thCellBorderColor};
   }
 `
 
@@ -76,10 +87,10 @@ export const GridHeaderItem = styled.div<
   overflow: hidden;
   align-items: center;
   line-height: 38px;
-  height: 58px;
+  height: ${() => useGridStyles().headerHeight};
   text-transform: none;
   z-index: 5;
-  padding: ${PADDING_CELL};
+  padding: ${() => useGridStyles().tdPadding};
 `
 
 export const GridHeaderItemSortable = styled.button<
@@ -101,6 +112,5 @@ export const NoItems = styled.div<ComponentPropsWithRef<'div'>>`
   display: flex;
   align-items: center;
   justify-content: center;
-  border: 1px solid
-    ${({ theme }: { theme: Theme }) => theme.semantic.color.border.secondary700};
+  border: 1px solid ${() => useGridStyles().thCellBorder};
 `
