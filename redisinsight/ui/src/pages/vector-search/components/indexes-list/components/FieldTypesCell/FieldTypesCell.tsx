d5d823107936a80372e30e176fcca2dbd0ab5096
Dijana Antovska <dijana.antovska@redis.com>
2026-02-06 14:44:27 +0200

    add cell components

---
+++ redisinsight/ui/src/pages/vector-search/components/indexes-list/components/FieldTypesCell/FieldTypesCell.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { FieldTag } from 'uiSrc/components/new-index/create-index-step/field-box/FieldTag'
+import { IIndexesListCell } from '../../IndexesList.types'
+import { FieldTypesWrapper } from './FIeldTypesCell.styles'
+
+const FieldTypesCell: IIndexesListCell = ({ row }) => {
+  const { id, fieldTypes } = row.original
+
+  return (
+    <FieldTypesWrapper data-testid={`index-field-types-${id}`}>
+      {fieldTypes.map((type) => (
+        <FieldTag
+          key={type}
+          tag={type}
+          dataTestId={`index-field-types-${id}--tag-${type}`}
+        />
+      ))}
+    </FieldTypesWrapper>
+  )
+}
+
+export default FieldTypesCell
