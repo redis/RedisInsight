474695ebbbe99ee2f12da45ab1d706e002fa8326
Valentin Kirilov <valentin.kirilov@redis.com>
2026-02-06 10:43:38 +0200

    feat(ui): add vector search skeleton pages
    - Introduced new pages for Vector Search, including `VectorSearchPage`, `VectorSearchCreateIndexPage`, and `VectorSearchQueryPage`.
    
    re #RI-7906

---
+++ redisinsight/ui/src/pages/vector-search/index.ts
@@ -0,0 +1,8 @@
+export { VectorSearchPage } from './pages/VectorSearchPage'
+export { VectorSearchQueryPage } from './pages/VectorSearchQueryPage'
+export { VectorSearchCreateIndexPage } from './pages/VectorSearchCreateIndexPage'
+export {
+  VectorSearchPageRouter,
+  default as VectorSearchPageRouterDefault,
+} from './VectorSearchPageRouter'
+export type { VectorSearchPageRouterProps } from './VectorSearchPageRouter.types'
