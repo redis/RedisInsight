b0bd37b4a5e4a3cf386a5cc1dbaeb039b5ac897e
Dijana Antovska <dijana.antovska@redis.com>
2026-02-13 09:49:39 +0200

    create reusable query results component

---
+++ redisinsight/ui/src/components/query/query-results/QueryResults.tsx
@@ -0,0 +1,164 @@
+import React from 'react'
+
+import { CodeButtonParams } from 'uiSrc/constants'
+import { ProfileQueryType } from 'uiSrc/pages/workbench/constants'
+import { generateProfileQueryForCommand } from 'uiSrc/pages/workbench/utils/profile'
+import { Nullable } from 'uiSrc/utils'
+import { CommandExecutionUI } from 'uiSrc/slices/interfaces'
+import { RunQueryMode, ResultsMode } from 'uiSrc/slices/interfaces/workbench'
+
+import { EmptyButton } from 'uiSrc/components/base/forms/buttons'
+import { DeleteIcon } from 'uiSrc/components/base/icons'
+import { ProgressBarLoader } from 'uiSrc/components/base/display'
+import QueryCard from '../query-card'
+
+import * as S from './QueryResults.styles'
+
+export interface QueryResultsProps {
+  isResultsLoaded: boolean
+  items: CommandExecutionUI[]
+  clearing: boolean
+  processing: boolean
+  activeMode: RunQueryMode
+  activeResultsMode?: ResultsMode
+  scrollDivRef: React.Ref<HTMLDivElement>
+  noResultsPlaceholder?: React.ReactNode
+  onToggleOpen?: (id: string, isOpen: boolean) => void
+  onQueryReRun: (
+    query: string,
+    commandId?: Nullable<string>,
+    executeParams?: CodeButtonParams,
+  ) => void
+  onQueryDelete: (commandId: string) => void
+  onAllQueriesDelete: () => void
+  onQueryOpen: (commandId: string) => void
+  onQueryProfile: (
+    query: string,
+    commandId?: Nullable<string>,
+    executeParams?: CodeButtonParams,
+  ) => void
+}
+
+const QueryResults = (props: QueryResultsProps) => {
+  const {
+    isResultsLoaded,
+    items = [],
+    clearing,
+    processing,
+    activeMode,
+    activeResultsMode,
+    noResultsPlaceholder,
+    onToggleOpen,
+    onQueryReRun,
+    onQueryProfile,
+    onQueryDelete,
+    onAllQueriesDelete,
+    onQueryOpen,
+    scrollDivRef,
+  } = props
+
+  const handleQueryProfile = (
+    profileType: ProfileQueryType,
+    commandExecution: {
+      command: string
+      mode?: RunQueryMode
+      resultsMode?: ResultsMode
+    },
+  ) => {
+    const { command, mode, resultsMode } = commandExecution
+    const profileQuery = generateProfileQueryForCommand(command, profileType)
+    if (profileQuery) {
+      onQueryProfile(profileQuery, null, {
+        mode,
+        results: resultsMode,
+        clearEditor: false,
+      })
+    }
+  }
+
+  return (
+    <S.Wrapper data-testid="query-results">
+      {!isResultsLoaded && (
+        <ProgressBarLoader
+          color="primary"
+          data-testid="progress-results-history"
+        />
+      )}
+      {!!items?.length && (
+        <S.Header>
+          <EmptyButton
+            size="small"
+            icon={DeleteIcon}
+            onClick={() => onAllQueriesDelete?.()}
+            disabled={clearing || processing}
+            data-testid="clear-history-btn"
+          >
+            Clear Results
+          </EmptyButton>
+        </S.Header>
+      )}
+      <S.Container>
+        <div ref={scrollDivRef} />
+        {items?.length
+          ? items.map(
+              ({
+                command = '',
+                isOpen = false,
+                result = undefined,
+                summary = undefined,
+                id = '',
+                loading,
+                createdAt,
+                mode,
+                resultsMode,
+                emptyCommand,
+                isNotStored,
+                executionTime,
+                db,
+              }) => (
+                <QueryCard
+                  id={id}
+                  key={id}
+                  isOpen={isOpen}
+                  result={result}
+                  summary={summary}
+                  clearing={clearing}
+                  loading={loading}
+                  command={command}
+                  createdAt={createdAt}
+                  activeMode={activeMode}
+                  emptyCommand={emptyCommand}
+                  isNotStored={isNotStored}
+                  executionTime={executionTime}
+                  mode={mode}
+                  activeResultsMode={activeResultsMode}
+                  resultsMode={resultsMode}
+                  db={db}
+                  onToggleOpen={onToggleOpen}
+                  onQueryOpen={() => onQueryOpen(id)}
+                  onQueryProfile={(profileType) =>
+                    handleQueryProfile(profileType, {
+                      command,
+                      mode,
+                      resultsMode,
+                    })
+                  }
+                  onQueryReRun={() =>
+                    onQueryReRun(command, null, {
+                      mode,
+                      results: resultsMode,
+                      clearEditor: false,
+                    })
+                  }
+                  onQueryDelete={() => onQueryDelete(id)}
+                />
+              ),
+            )
+          : null}
+        {isResultsLoaded && !items.length && (noResultsPlaceholder ?? null)}
+      </S.Container>
+    </S.Wrapper>
+  )
+}
+
+export default React.memo(QueryResults)
