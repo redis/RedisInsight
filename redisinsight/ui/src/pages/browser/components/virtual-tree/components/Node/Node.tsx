e701f9fa580ec0725a20bc3b1f8ceac2e343e130
ArtemHoruzhenko <artsiom.kharuzhenka@gmail.com>
2026-02-19 09:06:34 +0200

    do not show trash icon for folders when disabled by FF

--- redisinsight/ui/src/pages/browser/components/virtual-tree/components/Node/Node.tsx
+++ redisinsight/ui/src/pages/browser/components/virtual-tree/components/Node/Node.tsx
@@ -1,34 +1,35 @@
-import React, { useEffect, useState, useRef } from 'react'
-import { NodePublicState } from 'react-vtree/dist/es/Tree'
-import cx from 'classnames'
-import { useSelector } from 'react-redux'
+import React, { useEffect, useRef, useState } from 'react';
+import { NodePublicState } from 'react-vtree/dist/es/Tree';
+import cx from 'classnames';
+import { useSelector } from 'react-redux';
 
-import * as keys from 'uiSrc/constants/keys'
-import { Maybe } from 'uiSrc/utils'
+import * as keys from 'uiSrc/constants/keys';
+import { Maybe } from 'uiSrc/utils';
 import {
+  BrowserColumns,
+  FeatureFlags,
   KeyTypes,
   ModulesKeyTypes,
-  BrowserColumns,
-  TEXT_BULK_DELETE_TOOLTIP,
-  TEXT_BULK_DELETE_DISABLED_UNPRINTABLE,
   TEXT_BULK_DELETE_DISABLED_MULTIPLE_DELIMITERS,
-} from 'uiSrc/constants'
-import KeyRowTTL from 'uiSrc/pages/browser/components/key-row-ttl'
-import KeyRowSize from 'uiSrc/pages/browser/components/key-row-size'
-import KeyRowName from 'uiSrc/pages/browser/components/key-row-name'
-import KeyRowType from 'uiSrc/pages/browser/components/key-row-type'
-import { RedisResponseBuffer } from 'uiSrc/slices/interfaces'
-import { appContextDbConfig } from 'uiSrc/slices/app/context'
-import { RiIcon } from 'uiSrc/components/base/icons/RiIcon'
-import { RiTooltip } from 'uiSrc/components'
-import { IconButton } from 'uiSrc/components/base/forms/buttons'
-import { DeleteIcon } from 'uiSrc/components/base/icons'
-import { Flex, Row } from 'uiSrc/components/base/layout/flex'
-import { Text } from 'uiSrc/components/base/text'
-import styles from './styles.module.scss'
-import * as S from './Node.styles'
-import { TreeData } from '../../VirtualTree.types'
-import { DeleteKeyPopover } from '../../../delete-key-popover/DeleteKeyPopover'
+  TEXT_BULK_DELETE_DISABLED_UNPRINTABLE,
+  TEXT_BULK_DELETE_TOOLTIP,
+} from 'uiSrc/constants';
+import KeyRowTTL from 'uiSrc/pages/browser/components/key-row-ttl';
+import KeyRowSize from 'uiSrc/pages/browser/components/key-row-size';
+import KeyRowName from 'uiSrc/pages/browser/components/key-row-name';
+import KeyRowType from 'uiSrc/pages/browser/components/key-row-type';
+import { RedisResponseBuffer } from 'uiSrc/slices/interfaces';
+import { appContextDbConfig } from 'uiSrc/slices/app/context';
+import { RiIcon } from 'uiSrc/components/base/icons/RiIcon';
+import { FeatureFlagComponent, RiTooltip } from 'uiSrc/components';
+import { IconButton } from 'uiSrc/components/base/forms/buttons';
+import { DeleteIcon } from 'uiSrc/components/base/icons';
+import { Flex, Row } from 'uiSrc/components/base/layout/flex';
+import { Text } from 'uiSrc/components/base/text';
+import styles from './styles.module.scss';
+import * as S from './Node.styles';
+import { TreeData } from '../../VirtualTree.types';
+import { DeleteKeyPopover } from '../../../delete-key-popover/DeleteKeyPopover';
 
 const MAX_NESTING_LEVEL = 20
 
@@ -185,16 +186,18 @@ const Node = ({
           <S.FolderKeyCount data-testid={`count_${fullName}`}>
             {keyCount ?? ''}
           </S.FolderKeyCount>
-          <RiTooltip content={deleteTooltip} position="left">
-            <IconButton
-              icon={DeleteIcon}
-              onClick={handleDeleteFolder}
-              disabled={isDeleteDisabled}
-              className="showOnHoverKey"
-              aria-label="Delete Folder Keys"
-              data-testid={`delete-folder-btn-${fullName}`}
-            />
-          </RiTooltip>
+          <FeatureFlagComponent name={FeatureFlags.envDependent}>
+            <RiTooltip content={deleteTooltip} position="left">
+              <IconButton
+                icon={DeleteIcon}
+                onClick={handleDeleteFolder}
+                disabled={isDeleteDisabled}
+                className="showOnHoverKey"
+                aria-label="Delete Folder Keys"
+                data-testid={`delete-folder-btn-${fullName}`}
+              />
+            </RiTooltip>
+          </FeatureFlagComponent>
         </S.FolderActions>
       </Row>
     </RiTooltip>
