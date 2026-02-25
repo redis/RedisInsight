29917b854170e2a5e392ab18414bc888397fd550
Krum Tyukenov <krum.tyukenov@redis.com>
2026-02-25 18:20:09 +0200

    remove sentry from api

--- redisinsight/api/src/exceptions/global-exception.filter.ts
+++ redisinsight/api/src/exceptions/global-exception.filter.ts
@@ -1,7 +1,6 @@
 import { BaseExceptionFilter } from '@nestjs/core';
-import { ArgumentsHost, HttpException, Logger } from '@nestjs/common';
+import { ArgumentsHost, Logger } from '@nestjs/common';
 import { Request, Response } from 'express';
-import * as Sentry from '@sentry/node';
 
 export class GlobalExceptionFilter extends BaseExceptionFilter {
   private staticServerLogger = new Logger('GlobalExceptionFilter');
@@ -10,24 +9,9 @@ export class GlobalExceptionFilter extends BaseExceptionFilter {
     const ctx = host.switchToHttp();
     const request = ctx.getRequest<Request>();
 
-    // Capture exception to Sentry (if initialized)
-    // Skip HTTP exceptions with 4xx status codes as they are client errors
-    const isHttpException = exception instanceof HttpException;
-    const statusCode = isHttpException ? exception.getStatus() : 500;
-    const shouldReportToSentry = statusCode >= 500;
-
-    if (shouldReportToSentry) {
-      Sentry.captureException(exception, {
-        extra: {
-          url: request.url,
-          method: request.method,
-          statusCode,
-        },
-      });
-    }
-
     if (/^\/(?:plugins|static)\//i.test(request.url)) {
       const response = ctx.getResponse<Response>();
+      const statusCode = exception['statusCode'] || 500;
       const message = `Error when trying to fetch ${request.url}`;
 
       this.staticServerLogger.error(message, { ...exception } as any);
