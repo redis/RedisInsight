888e941aacf9ca01dc31d031f0de3286fcaa9984
Pavel Angelov <pavel.angelov@redis.com>
2026-03-02 15:31:46 +0200

    this

--- tests/e2e-playwright/pages/index.ts
+++ tests/e2e-playwright/pages/index.ts
@@ -3,6 +3,7 @@ export { InstancePage } from './InstancePage';
 export { InstanceHeader, NavigationTabs, BottomPanel } from './components';
 export { BrowserPage, AddKeyDialog, KeyList } from './browser';
 export { CliPanel } from './cli';
+export { CommandHelperPanel } from './command-helper';
 export {
   DatabasesPage,
   AddDatabaseDialog,
