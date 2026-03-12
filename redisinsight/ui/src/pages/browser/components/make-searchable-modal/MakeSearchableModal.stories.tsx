8566f3f48afb1a75c129e4e28688a73483a9519a
Dijana Antovska <dijana.antovska@redis.com>
2026-03-12 10:45:24 +0200

    add empty onConfirm and onCancel

--- redisinsight/ui/src/pages/browser/components/make-searchable-modal/MakeSearchableModal.stories.tsx
+++ redisinsight/ui/src/pages/browser/components/make-searchable-modal/MakeSearchableModal.stories.tsx
@@ -1,10 +1,15 @@
 import type { Meta, StoryObj } from '@storybook/react-vite'
+import { fn } from 'storybook/test'
 
 import { MakeSearchableModal } from './MakeSearchableModal'
 
 const meta: Meta<typeof MakeSearchableModal> = {
   component: MakeSearchableModal,
   tags: ['autodocs'],
+  args: {
+    onConfirm: fn(),
+    onCancel: fn(),
+  },
 }
 
 export default meta
