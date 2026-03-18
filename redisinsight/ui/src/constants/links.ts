41aa89f9a94ea0b5ea0791151922a9440fedebaf
Dijana Antovska <dijana.antovska@redis.com>
2026-03-18 11:01:32 +0200

    remove unused import

--- redisinsight/ui/src/constants/links.ts
+++ redisinsight/ui/src/constants/links.ts
@@ -1,4 +1,4 @@
-import { CloudSsoUtmCampaign, OAuthSocialSource } from 'uiSrc/slices/interfaces'
+import { OAuthSocialSource } from 'uiSrc/slices/interfaces'
 
 export const EXTERNAL_LINKS = {
   redisIo: 'https://redis.io',
