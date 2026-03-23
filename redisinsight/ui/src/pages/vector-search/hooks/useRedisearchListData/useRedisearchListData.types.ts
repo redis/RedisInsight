5a644522370c675d3d85a638432650ba0cb62db7
Valentin Kirilov <valentin.kirilov@redis.com>
2026-03-23 11:08:15 +0200

    Redirect to index list when vector search query page references a non-existent index (#5682)
    * fix(ui): enhance VectorSearchQueryPage to handle index existence checks

--- redisinsight/ui/src/pages/vector-search/hooks/useRedisearchListData/useRedisearchListData.types.ts
+++ redisinsight/ui/src/pages/vector-search/hooks/useRedisearchListData/useRedisearchListData.types.ts
@@ -2,6 +2,7 @@ import { RedisResponseBuffer } from 'uiSrc/slices/interfaces'
 
 export interface UseRedisearchListDataReturn {
   loading: boolean | undefined
+  error: string
   data: RedisResponseBuffer[]
   stringData: string[]
 }
