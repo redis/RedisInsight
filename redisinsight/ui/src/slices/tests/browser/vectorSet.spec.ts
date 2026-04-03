031894f7cac931afe7deca4439177fa2199acf1f
Dijana Antovska <dijana.antovska@redis.com>
2026-04-03 17:26:10 +0300

    add tests for the delete thunk and actions

--- redisinsight/ui/src/slices/tests/browser/vectorSet.spec.ts
+++ redisinsight/ui/src/slices/tests/browser/vectorSet.spec.ts
@@ -5,18 +5,26 @@ import {
   cleanup,
   initialStateDefault,
   mockedStore,
+  mockStore,
 } from 'uiSrc/utils/test-utils'
 import {
   addErrorNotification,
+  addMessageNotification,
   IAddInstanceErrorPayload,
 } from 'uiSrc/slices/app/notifications'
+import successMessages from 'uiSrc/components/notifications/success-messages'
 import { MOCK_TIMESTAMP } from 'uiSrc/mocks/data/dateNow'
 import {
   vectorSetElementFactory,
   vectorSetTestKeyName,
   vectorSetPaginationCursorAfter,
 } from 'uiSrc/mocks/factories/browser/vectorSet/vectorSetElement.factory'
-import { updateSelectedKeyRefreshTime } from '../../browser/keys'
+import {
+  deletePatternKeyFromList,
+  deleteSelectedKeySuccess,
+  refreshKeyInfo,
+  updateSelectedKeyRefreshTime,
+} from '../../browser/keys'
 import reducer, {
   initialState,
   loadVectorSetElements,
@@ -25,9 +33,14 @@ import reducer, {
   loadMoreVectorSetElements,
   loadMoreVectorSetElementsSuccess,
   loadMoreVectorSetElementsFailure,
+  removeVectorSetElements,
+  removeVectorSetElementsSuccess,
+  removeVectorSetElementsFailure,
+  removeElementsFromList,
   vectorSetSelector,
   fetchVectorSetElements,
   fetchMoreVectorSetElements,
+  deleteVectorSetElements,
 } from '../../browser/vectorSet'
 
 jest.mock('uiSrc/services', () => ({
@@ -305,6 +318,89 @@ describe('vectorSet slice', () => {
     })
   })
 
+  describe('removeVectorSetElements', () => {
+    it('should properly set the state before delete', () => {
+      const state = {
+        ...initialState,
+        loading: true,
+      }
+
+      const nextState = reducer(initialState, removeVectorSetElements())
+
+      const rootState = {
+        ...initialStateDefault,
+        browser: { ...initialStateDefault.browser, vectorSet: nextState },
+      }
+      expect(vectorSetSelector(rootState)).toEqual(state)
+    })
+  })
+
+  describe('removeVectorSetElementsSuccess', () => {
+    it('should properly set the state after successful delete', () => {
+      const prevState = {
+        ...initialState,
+        loading: true,
+      }
+
+      const state = {
+        ...initialState,
+        loading: false,
+      }
+
+      const nextState = reducer(prevState, removeVectorSetElementsSuccess())
+
+      const rootState = {
+        ...initialStateDefault,
+        browser: { ...initialStateDefault.browser, vectorSet: nextState },
+      }
+      expect(vectorSetSelector(rootState)).toEqual(state)
+    })
+  })
+
+  describe('removeVectorSetElementsFailure', () => {
+    it('should properly set the error after failed delete', () => {
+      const data = 'some error'
+      const state = {
+        ...initialState,
+        loading: false,
+        error: data,
+      }
+
+      const nextState = reducer(
+        initialState,
+        removeVectorSetElementsFailure(data),
+      )
+
+      const rootState = {
+        ...initialStateDefault,
+        browser: { ...initialStateDefault.browser, vectorSet: nextState },
+      }
+      expect(vectorSetSelector(rootState)).toEqual(state)
+    })
+  })
+
+  describe('removeElementsFromList', () => {
+    it('should filter out deleted elements and decrement total', () => {
+      const elements = vectorSetElementFactory.buildList(3)
+      const stateWithElements = {
+        ...initialState,
+        data: {
+          ...initialState.data,
+          total: 3,
+          elements,
+        },
+      }
+
+      const nextState = reducer(
+        stateWithElements,
+        removeElementsFromList([elements[0].name]),
+      )
+
+      expect(nextState.data.elements).toHaveLength(2)
+      expect(nextState.data.total).toEqual(2)
+    })
+  })
+
   describe('thunks', () => {
     describe('fetchVectorSetElements', () => {
       const thunkElements = vectorSetElementFactory.buildList(3)
@@ -416,6 +512,111 @@ describe('vectorSet slice', () => {
           loadMoreVectorSetElements(),
           addErrorNotification(responsePayload as IAddInstanceErrorPayload),
           loadMoreVectorSetElementsFailure(errorMessage),
+        ]
+
+        expect(mockedStore.getActions()).toEqual(expectedActions)
+      })
+    })
+
+    describe('deleteVectorSetElements', () => {
+      const key = 'key'
+      const elements = ['elem1', 'elem2']
+
+      it('should dispatch success actions when delete is successful', async () => {
+        const responsePayload = { status: 200, data: { affected: 2 } }
+        apiService.delete = jest.fn().mockResolvedValue(responsePayload)
+
+        const nextState = {
+          ...initialStateDefault,
+          browser: {
+            ...initialStateDefault.browser,
+            vectorSet: {
+              ...initialState,
+              data: {
+                ...initialState.data,
+                total: 10,
+              },
+            },
+          },
+        }
+
+        const storeWithData = mockStore(nextState)
+
+        await storeWithData.dispatch<any>(
+          deleteVectorSetElements(key as any, elements as any),
+        )
+
+        const expectedActions = [
+          removeVectorSetElements(),
+          removeVectorSetElementsSuccess(),
+          removeElementsFromList(elements),
+          refreshKeyInfo(),
+          addMessageNotification(
+            successMessages.REMOVED_KEY_VALUE(key, elements.join(''), 'Member'),
+          ),
+        ]
+
+        expect(
+          storeWithData.getActions().slice(0, expectedActions.length),
+        ).toEqual(expectedActions)
+      })
+
+      it('should dispatch delete key actions when last elements are removed', async () => {
+        const responsePayload = { status: 200, data: { affected: 2 } }
+        apiService.delete = jest.fn().mockResolvedValue(responsePayload)
+
+        const nextState = {
+          ...initialStateDefault,
+          browser: {
+            ...initialStateDefault.browser,
+            vectorSet: {
+              ...initialState,
+              data: {
+                ...initialState.data,
+                total: 2,
+              },
+            },
+          },
+        }
+
+        const storeWithData = mockStore(nextState)
+
+        await storeWithData.dispatch<any>(
+          deleteVectorSetElements(key as any, elements as any),
+        )
+
+        const expectedActions = [
+          removeVectorSetElements(),
+          removeVectorSetElementsSuccess(),
+          removeElementsFromList(elements),
+          deleteSelectedKeySuccess(),
+          deletePatternKeyFromList(key),
+          addMessageNotification(successMessages.DELETED_KEY(key)),
+        ]
+
+        expect(storeWithData.getActions()).toEqual(expectedActions)
+      })
+
+      it('should dispatch failure actions when delete fails', async () => {
+        const errorMessage = 'Something was wrong!'
+        const responsePayload = {
+          response: {
+            status: 500,
+            data: { message: errorMessage },
+          },
+        }
+        apiService.delete = jest.fn().mockRejectedValue(responsePayload)
+
+        await store.dispatch<any>(
+          deleteVectorSetElements(key as any, elements as any),
+        )
+
+        const expectedActions = [
+          removeVectorSetElements(),
+          addErrorNotification(
+            responsePayload as unknown as IAddInstanceErrorPayload,
+          ),
+          removeVectorSetElementsFailure(errorMessage),
         ]
 
         expect(mockedStore.getActions()).toEqual(expectedActions)
