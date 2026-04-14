40b560f88b202ba928d249cdbf3350801c09c7b1
dantovska <dijana.antovska@redis.com>
2026-04-14 15:53:06 +0300

    RI-8164: Element details components (#5759)
    * create element details hook
    * create element details drawer component
    * update element list to support view element details feature
    * update vector set details component to support element details

--- redisinsight/ui/src/pages/browser/modules/key-details/components/vector-set-details/VectorSetDetails.tsx
+++ redisinsight/ui/src/pages/browser/modules/key-details/components/vector-set-details/VectorSetDetails.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 import { selectedKeySelector } from 'uiSrc/slices/browser/keys'
-
 import {
   KeyDetailsHeader,
   KeyDetailsHeaderProps,
 } from 'uiSrc/pages/browser/modules'
 import { VectorSetElementList } from './vector-set-element-list'
 import { VectorSetKeySubheader } from './vector-set-key-subheader'
+import { ElementDetails } from './element-details'
+import { useElementDetails } from './hooks'
 import * as S from './VectorSetDetails.styles'
 
 export interface Props extends KeyDetailsHeaderProps {
@@ -21,6 +22,14 @@ const VectorSetDetails = (props: Props) => {
   const { onRemoveKey } = props
   const { loading } = useSelector(selectedKeySelector)
 
+  const {
+    viewedElement,
+    isDetailsPanelOpen,
+    handleViewElement,
+    handleClosePanel,
+    handleDrawerDidClose,
+  } = useElementDetails()
+
   return (
     <S.Container>
       <KeyDetailsHeader {...props} key="key-details-header" />
@@ -28,10 +37,19 @@ const VectorSetDetails = (props: Props) => {
       <S.DetailsBody>
         {!loading && (
           <S.ListWrapper>
-            <VectorSetElementList onRemoveKey={onRemoveKey} />
+            <VectorSetElementList
+              onRemoveKey={onRemoveKey}
+              onViewElement={handleViewElement}
+            />
           </S.ListWrapper>
         )}
       </S.DetailsBody>
+      <ElementDetails
+        element={viewedElement}
+        isOpen={isDetailsPanelOpen}
+        onClose={handleClosePanel}
+        onDrawerDidClose={handleDrawerDidClose}
+      />
     </S.Container>
   )
 }
---
+++ redisinsight/ui/src/pages/browser/modules/key-details/components/vector-set-details/element-details/ElementDetails.styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+import { Col } from 'uiSrc/components/base/layout/flex'
+import { TextArea } from 'uiSrc/components/base/inputs'
+import { IconButton } from 'uiSrc/components/base/forms/buttons'
+
+export const Body = styled(Col)`
+  gap: ${({ theme }) => theme.core?.space?.space400};
+  padding-top: ${({ theme }) => theme.core?.space?.space100};
+`
+
+export const VectorTextArea = styled(TextArea)`
+  font-family: 'Source Code Pro';
+  border: 1px solid ${({ theme }) => theme.semantic?.color?.border?.neutral500};
+  padding: ${({ theme }) => theme.core?.space?.space020};
+  scrollbar-width: thin;
+`
+
+export const EditorWrapper = styled.div<{ children: React.ReactNode }>`
+  position: relative;
+  width: 100%;
+  height: 200px;
+  border: 1px solid ${({ theme }) => theme.semantic?.color?.border?.neutral500};
+`
+
+export const EditButton = styled(IconButton)`
+  position: absolute;
+  top: ${({ theme }) => theme.core?.space?.space150};
+  right: ${({ theme }) => theme.core?.space?.space200};
+  z-index: 1;
+`
