6750a86bd5349b0eaa302d2bd618c8cdf2f541e4
Dijana Antovska <dijana.antovska@redis.com>
2026-04-22 09:12:55 +0300

    add factory for adding vector set element data

--- redisinsight/ui/src/mocks/factories/browser/vectorSet/vectorSetElement.factory.ts
+++ redisinsight/ui/src/mocks/factories/browser/vectorSet/vectorSetElement.factory.ts
@@ -2,7 +2,10 @@ import { faker } from '@faker-js/faker'
 import { Factory } from 'fishery'
 import { KeyTypes } from 'uiSrc/constants'
 import { stringToBuffer } from 'uiSrc/utils'
-import { VectorSetElement } from 'uiSrc/slices/interfaces'
+import {
+  AddVectorSetElementsData,
+  VectorSetElement,
+} from 'uiSrc/slices/interfaces'
 
 export const mockKeyBuffer = stringToBuffer(faker.word.noun())
 
@@ -34,6 +37,24 @@ export const vectorSetElementWithAttributesFactory =
   Factory.define<VectorSetElement>(() => ({
     ...buildBaseElement(),
     attributes: mockVectorSetElementAttributes(),
+  }))
+
+const buildMockVector = (dim = 3): number[] =>
+  Array.from({ length: dim }, () =>
+    faker.number.float({ min: -1, max: 1, fractionDigits: 6 }),
+  )
+
+export const addVectorSetElementsDataFactory =
+  Factory.define<AddVectorSetElementsData>(() => ({
+    keyName: stringToBuffer(`vset:${faker.string.alphanumeric(10)}`),
+    elements: [
+      { name: faker.word.noun(), vector: buildMockVector() },
+      {
+        name: faker.word.noun(),
+        vector: buildMockVector(),
+        attributes: mockVectorSetElementAttributes(),
+      },
+    ],
   }))
 
 /** Redis key name string for vector set tests (stable shape, random value). */
