986e33f79b3c67cc7b449cce2e7969e6305b4935
Valentin Kirilov <valentin.kirilov@redis.com>
2026-05-07 18:22:19 +0300

    Revert "chore(deps): bump styled-components from 5.3.11 to 6.4.1 (#5841)"
    This reverts commit e2a7d8d133e98b5582f6391b338b51d68f63f81e.

--- redisinsight/ui/src/components/base/inputs/SwitchInput.spec.tsx
+++ redisinsight/ui/src/components/base/inputs/SwitchInput.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-
-import { render, userEvent } from 'uiSrc/utils/test-utils'
+import userEvent from '@testing-library/user-event'
+import { render } from '@testing-library/react'
 
 import SwitchInput from './SwitchInput'
 
