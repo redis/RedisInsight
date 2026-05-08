f8f32b1deca35334fa4289f0cfdac42579b7e618
Dijana Antovska <dijana.antovska@redis.com>
2026-05-08 06:58:01 +0300

    add reply stride constant

---
+++ redisinsight/api/src/modules/browser/vector-set/constants.ts
@@ -0,0 +1,5 @@
+/**
+ * Stride of `VSIM ... WITHSCORES WITHATTRIBS` replies. The flat reply array
+ * contains 3 entries per match in the order `(name, score, attributes|null)`.
+ */
+export const VSIM_REPLY_STRIDE = 3;
