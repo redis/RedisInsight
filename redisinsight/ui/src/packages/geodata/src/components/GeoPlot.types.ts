b1fc0e44a2285cce133ef31b602911febc1cd6db
Pierre Lambert <pierre.lambert.pro@gmail.com>
2026-05-19 19:42:16 +0200

    refactor: split geodata plot types

---
+++ redisinsight/ui/src/packages/geodata/src/components/GeoPlot.types.ts
@@ -0,0 +1,23 @@
+import * as L from 'leaflet'
+
+import { GeoResult, ParsedGeoCommand } from '../types'
+
+export interface GeoPlotProps {
+  mode: 'markers' | 'heatmap'
+  results: GeoResult[]
+  command: ParsedGeoCommand
+}
+
+export interface DistanceThresholds {
+  close: number
+  middle: number
+}
+
+export interface DistanceMarkerOptions extends L.CircleMarkerOptions {
+  distanceKm?: number
+}
+
+export interface ThresholdControlsProps {
+  thresholds: DistanceThresholds
+  onChange: (thresholds: DistanceThresholds) => void
+}
